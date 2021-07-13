07c50bfb4d0d407777960b75268e442ed9557f33
AlexeyChekalin <sphinxsk@yandex.ru>
2021-07-14 02:55:33 +0300

    make validation form

--- layout/src/modules/sendForm.js
+++ layout/src/modules/sendForm.js
@@ -1,14 +1,59 @@
 class SendForm {
-  constructor(selector) {
+  constructor(selector, errorClassName = 'send-form-message-error') {
     this.forms = document.querySelectorAll(selector);
+    this.errorClassName = errorClassName;
+  }
+
+  getErorMesage(message) {
+    const errorMessage = document.createElement('div');
+    errorMessage.className = this.errorClassName;
+    errorMessage.textContent = message;
+    return errorMessage;
+  }
+
+  validate(form) {
+    const inputs = form.querySelectorAll('input');
+    let result = inputs.length;
+
+    inputs.forEach(elem => {
+      if (elem.getAttribute('type') === 'checkbox') {
+        if (elem.checked) {
+          result--;
+        } else {
+          const errorMessage = this.getErorMesage.call(this, 'Подтвердите согласие');
+          elem.insertAdjacentElement('afterend', errorMessage);
+          setTimeout(() => errorMessage.remove(), 1000);
+        }
+      }
+      if (elem.name === 'name') {
+        if (elem.value.length > 2) {
+          result--;
+        } else {
+          const errorMessage = this.getErorMesage.call(this, 'Введите коректное значение');
+          elem.insertAdjacentElement('afterend', errorMessage);
+          setTimeout(() => errorMessage.remove(), 1000);
+        }
+      }
+      if (elem.name === 'phone') {
+        if (elem.value.length > 17) {
+          result--;
+        } else {
+          const errorMessage = this.getErorMesage.call(this, 'Введите коректное значение');
+          elem.insertAdjacentElement('afterend', errorMessage);
+          setTimeout(() => errorMessage.remove(), 1000);
+        }
+      }
+    });
+    return result === 0;
   }
 
   sendForm(form) {
     event.preventDefault();
+    if (!this.validate.call(this, form)) return;
     if (form.querySelector('[type="checkbox"]').checked) {
       const formData = new FormData(form);
       const body = {};
-  
+
       formData.forEach((val, key) => {
         body[key] = val;
       });
@@ -31,11 +76,11 @@ class SendForm {
     });
   }
 
-
-
-
   init() {
-    this.forms.forEach(form => form.addEventListener('submit', this.sendForm.bind(this, form)));
+    this.forms.forEach(form => {
+      form.querySelectorAll('input').forEach(elem => elem.removeAttribute('required'));
+      form.addEventListener('submit', this.sendForm.bind(this, form));
+    });
   }
 }
 
